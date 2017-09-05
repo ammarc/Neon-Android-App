71de6b5a412a4db20b9e12d978af3473eda026c1
ammarc <ammara@student.unimelb.edu.au>
2017-09-05 15:09:06 +1000

    adding facebook

--- app/src/main/java/itproject/neon_client/LoginActivity.java
+++ app/src/main/java/itproject/neon_client/LoginActivity.java
@@ -1,7 +1,11 @@
 package itproject.neon_client;
 
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.View;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import android.animation.Animator;
@@ -13,6 +17,8 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 
 
 import java.util.ArrayList;
@@ -25,10 +31,38 @@ import java.util.List;
 public class LoginActivity extends Activity
 {
 
+    private CallbackManager callbackManager;
+
     /** Called when the user taps the Send button */
     public void loginButton(View view) {
         // Do something in response to button
         Intent intent = new Intent(this, SecondPageActivity.class);
         startActivity(intent);
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
     }
 }
