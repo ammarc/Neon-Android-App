ae9291cf66ddf093536cb036a23c0931b689d784
LachieThomas <31024414+LachieThomas@users.noreply.github.com>
2017-10-23 00:45:54 +1100

    updated helper files

--- app/src/main/java/itproject/neon_client/helpers/MapHelper.java
+++ app/src/main/java/itproject/neon_client/helpers/MapHelper.java
@@ -14,6 +14,13 @@ public class MapHelper {
     private static final String SERVER_ADDRESS = "http://13.65.209.193:3000";
     private static final String TAG = "testing";
 
+    /**
+     * Method returns the latitude of a user when requested from a friend
+     * @param to_user is the username of a user who's latitude is being requested
+     * @param from_user is the username of the user who is requesting the latitude
+     * @return the friends latitude
+     * @throws JSONException
+     */
     public static double get_latitude(String to_user, String from_user) throws JSONException {
         String path = SERVER_ADDRESS + "/gps/friends?user=" + from_user;
         JSONArray friends_locations = DatabaseConnect.get(path);
@@ -31,6 +38,13 @@ public class MapHelper {
         throw new JSONException("Could not find friend's latitude");
     }
 
+    /**
+     * Method returns the longitude of a user when requested from a friend
+     * @param to_user is the username of a user who's longitude is being requested
+     * @param from_user is the username of the user who is requesting the longitude
+     * @return the friends longitude
+     * @throws JSONException
+     */
     public static double get_longitude(String to_user, String from_user) throws JSONException {
         String path = SERVER_ADDRESS + "/gps/friends?user=" + from_user;
         JSONArray friends_locations = DatabaseConnect.get(path);
@@ -48,6 +62,12 @@ public class MapHelper {
         throw new JSONException("Could not find friend's longitude");
     }
 
+    /**
+     * method to update a users location in the database
+     * @param username is the username of the user whos location is being updated
+     * @param latitude is the latitude of the user
+     * @param longitude is the longitude of the user
+     */
     public static void post_location(String username, double latitude, double longitude) {
       String post_message = "{\"username\":\"" + username +
                 "\",\"latitude\":\"" + latitude +
@@ -59,6 +79,11 @@ public class MapHelper {
         DatabaseConnect.post(field);
     }
 
+    /**
+     * method to send a location permission request from one user to another
+     * @param from_user is the username of the friend sending the permission request
+     * @param to_user is the username of the friend receiveng the permission request
+     */
     public static void request_permission(String from_user, String to_user) {
         String path = SERVER_ADDRESS + "/gps/request";
         String post_message = "{\"to_user\":\"" + to_user + "\",\"from_user\":\"" + from_user + "\"}";
@@ -66,6 +91,11 @@ public class MapHelper {
         DatabaseConnect.post(field);
     }
 
+    /**
+     * method to accept a location permission request
+     * @param from_user is the username of the user who sent the location permission request
+     * @param to_user is the username of the user who is accepting the permission request
+     */
     public static void accept_permission_request(String from_user, String to_user) {
         String patch_message = "{\"to_user\":\"" + to_user + "\",\"from_user\":\"" + from_user + "\"}";
         String path = SERVER_ADDRESS + "/gps/request/accept";
@@ -73,6 +103,13 @@ public class MapHelper {
         DatabaseConnect.post(field);
     }
 
+    /**
+     * method to get the current location permission status between two friends
+     * @param from_user is the user who we are checking whether they have permission or not
+     * @param to_user is the user who we are checking if they have given permission or not
+     * @return -1 if they do not have permission, 1 if they do have permission
+     * @throws JSONException
+     */
     public static int get_permission_status(String from_user, String to_user) throws JSONException{
         String path = SERVER_ADDRESS + "/gps/friends/status?from_user=" + from_user + "&to_user=" + to_user;
         JSONArray friendship = DatabaseConnect.get(path);
@@ -84,6 +121,11 @@ public class MapHelper {
         return friendship.getJSONObject(0).getInt("location_status");
     }
 
+    /**
+     * method to get a list of all users who have requested location permission
+     * @param username is the username of the user who has received permission requests
+     * @return an arraylist of all of the usernames of those that have requested permission
+     */
     public static ArrayList<String> get_permission_requests(String username) {
         try {
             ArrayList<String> requests = new ArrayList<String>();
